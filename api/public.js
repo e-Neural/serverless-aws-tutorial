948f87acc04c93cca47a833185f91f301f4cb719
e-Neural <alexandre@keepsoftware.com>
2021-10-08 12:30:01 -0300

    implementando auth

---
+++ api/public.js
@@ -0,0 +1,38 @@
+'use strict';
+const AWS = require('aws-sdk');
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.list = (event, context, callback) => {
+    console.log("Receieved request to list all candidates. Event is", event);
+    var params = {
+        TableName: process.env.CANDIDATE_TABLE,
+        ProjectionExpression: "id, fullname, email"
+    };
+    const onScan = (err, data) => {
+        if (err) {
+            console.log('Scan failed to load data. Error JSON:', JSON.stringify(err, null, 2));
+            callback(err);
+        } else {
+            console.log("Scan succeeded.");
+            return callback(null, successResponseBuilder(JSON.stringify({
+                    candidates: data.Items
+                })
+            ));
+        }
+    };
+    dynamoDb.scan(params, onScan);
+};
+
+const successResponseBuilder = (body) => {
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: body
+    };
+};
